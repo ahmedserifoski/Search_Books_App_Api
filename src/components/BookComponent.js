efd9c83fdd3509c6acefc05454e6577a6cf41449
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2020-09-21 20:42:41 +0200

    adjust book background

--- src/components/BookComponent.js
+++ src/components/BookComponent.js
@@ -1,15 +1,3 @@
-// import React from 'react'
-
-// const BookComponent = ({book}) => {
-//     return (
-//         <div >
-//             <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="Desc"/>
-//             <p>{book.volumeInfo.title}</p>
-//         </div>
-//     )
-// }
-
-// export default BookComponent
 
 import React from 'react'
 import "./BookComponent.css"
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,68 +1,14 @@
-// import React, {useState} from 'react'
-// import BookComponent from "./BookComponent"
-// const SearchBooks = () => {
-
-//     const [query, setQuery] = useState("")
-//     const [books, setBooks] = useState([])
-
-//     const searchBooks = async (event) => {
-//         event.preventDefault()
-
-        
-
-//         const key = "AIzaSyCBqv8O6IeVjogiljjDy7r2Kr3XdTxun0Q"
-//         const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${key}`
-        
-//         try{
-//             const response = await fetch(url)
-//             const data = await response.json()
-//             console.log(data.items)
-//             setBooks(data.items)
-            
-//         }catch(err){
-//             console.error(err)
-//         }
-        
-//     }
-
-//     return (
-//         <div>
-//             <form className="form" onSubmit={searchBooks}>
-//                 <label className="label" htmlFor="query">Book Title: </label>
-//                 <input 
-//                     className="input" 
-//                     type="text" 
-//                     name="query" 
-//                     value={query}
-//                     onChange={(event) => setQuery(event.target.value)}
-//                     placeholder="Search Books"
-//                 />
-//                 <button className="button" type="submit">Search</button>
-//             </form>
-//             <div>
-//                 {books.map(book => (
-//                     <BookComponent book={book} key={book.id}/>
-//                 ))}
-//             </div>
-//         </div>
-        
-//     )
-// }
-
-// export default SearchBooks
 
 
 import React, {useState} from 'react'
 import BookComponent from "./BookComponent"
 import "./SearchBooks.css"
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const SearchBooks = () => {
 
-    
-    
     const [query, setQuery] = useState("")
     const [books, setBooks] = useState([])
 
@@ -77,12 +23,13 @@ const SearchBooks = () => {
         const res = await fetch(url)
         const data = await res.json()
         setBooks(data.items)
-        console.log(data.items)
+        // console.log(data.items)
     }
+
+    
 
     return (
         <div>
-            
             <form className="form" onSubmit={searchBooks}>
                 <input 
                 className="input"
