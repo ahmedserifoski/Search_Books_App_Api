1274623d6f40bb8f0c47b4eb6fa690d0b66e1778
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2020-09-23 19:29:53 +0200

    add initial search

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,6 +1,6 @@
 
 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import BookComponent from "./BookComponent"
 import "./SearchBooks.css"
 
@@ -12,10 +12,22 @@ const SearchBooks = () => {
     const [query, setQuery] = useState("")
     const [books, setBooks] = useState([])
 
+    useEffect(() => {
+        searchRandomBooks()
+    }, [])
+
+    const searchRandomBooks = async () => {
+        const key = "AIzaSyCBqv8O6IeVjogiljjDy7r2Kr3XdTxun0Q"
+        const url = `https://www.googleapis.com/books/v1/volumes?q=gameofthrones&key=${key}`
+
+        const res = await fetch(url)
+        const data = await res.json()
+        setBooks(data.items)
+        // console.log(data.items)
+    }
+
     const searchBooks = async (event) => {
         event.preventDefault()
-
-        document.querySelector(".question").setAttribute("style", "display: none")
 
         const key = "AIzaSyCBqv8O6IeVjogiljjDy7r2Kr3XdTxun0Q"
         const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${key}`
@@ -40,8 +52,7 @@ const SearchBooks = () => {
                 onChange={(event => setQuery(event.target.value))}
             />
             <button className="button" type="submit">Search</button>
-            </form>        
-            <h2 className="question">What would you like to read</h2>
+            </form>
             <div className="books">
                 {books.map(book => (
                     <BookComponent book={book} key={book.id} />
