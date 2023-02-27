1fdfab5f2a4f4374aa23e2d79750ef38e2083da3
Ahmed Serifoski <ahmedserifoski@Ahmeds-MacBook-Air.local>
2023-02-27 23:35:57 +0100

    add google books api

--- src/components/BookComponent.js
+++ src/components/BookComponent.js
@@ -1,18 +1,27 @@
     
-import React from 'react'
+import React, {useState} from 'react'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from 'react-bootstrap/Button';
+
 import "./BookComponent.css"
 
-
-
 const BookComponent = ({book}) => {
+    const [open, setOpen] = useState(false);
 
-    return (<div className="book">
-            {book.image_url ? <img className="image" src={book.image_url} alt=""/> : ""}
-            <h4 className="title">{book.title}</h4>
-            {/*<h3 className="subtitle">{book.subtitle}</h3>*/}
-            <small className="authors">by: <strong>{book.author.name}</strong></small>
-            {/*<p className="pageNumber">Page number:  <strong>{book.pageCount}</strong></p>*/}
-            {/* <button onClick={getDesc}>Search more</button> */}
+    return (<div className="book" style={{ maxHeight: open == false? '2000px': 'auto'}} >
+            {book.volumeInfo.imageLinks ? <img className="image" src={book.volumeInfo.imageLinks.smallThumbnail} alt=""/> : ""}
+            <h4 className="title">{book.volumeInfo.title}</h4>
+            <small className="authors">by: <strong>{book.volumeInfo.authors}</strong></small>
+            <p className="pageNumber">Page number:  <strong>{book.volumepageCount}</strong></p>
+            <Button 
+                variant="light"
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+            >
+                Show More
+            </Button>{' '}
         </div>)
 }
 
---
+++ src/components/DetailedBook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailedBook = () => {
+  return (
+    <div>DetailedBook</div>
+  )
+}
+
+export default DetailedBook
